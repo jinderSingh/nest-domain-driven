4f24b045d7717a73755052eb2439288775319c62
jinderSingh <kuljindergojlia@gmail.es>
2021-06-27 20:43:18 +0200

    add nest/config dependency to make use of dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,13 @@ import { PersistanceModule } from './persistence/persistance.module';
 import { DomainModule } from './domain/domain.module';
 import { ApiModule } from './api/api.module';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DomainModule.forRoot({ imports: [PersistanceModule] }), ApiModule],
+  imports: [
+    DomainModule.forRoot({ imports: [PersistanceModule] }),
+    ApiModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
