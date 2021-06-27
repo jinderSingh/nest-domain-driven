3b31b20c77aede12ca400c2bf7b64eb62bd3d6d3
jinderSingh <kuljindergojlia@gmail.es>
2021-06-27 20:43:39 +0200

    configure mongodb connection

--- src/persistence/persistance.module.ts
+++ src/persistence/persistance.module.ts
@@ -1,12 +1,24 @@
 import { TodoPersistanceAdapter } from './adapters/todo.adapter';
 import { TodoAdapter } from '../domain/adapters/persistance/todo.adapter';
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 const providers = [
   { provide: TodoAdapter.INJECTION_KEY, useClass: TodoPersistanceAdapter },
 ];
 
 @Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('DB_URI'),
+        user: configService.get('DB_USER'),
+        pass: configService.get('DB_PWD'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers,
   exports: [...providers],
 })
