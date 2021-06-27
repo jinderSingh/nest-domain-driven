5defd84f9eb15595fd7a68f21e4c62a374e6054d
jinderSingh <kuljindergojlia@gmail.es>
2021-06-27 18:24:42 +0200

    add exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { PersistenceExceptionFilter } from './api/filters/exception/persistence-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new PersistenceExceptionFilter());
   await app.listen(3000);
 }
 bootstrap();
