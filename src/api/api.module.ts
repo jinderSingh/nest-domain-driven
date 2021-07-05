92daf060e281a6664e3e9253488b98b4252f1930
jinderSingh <kuljindergojlia@gmail.es>
2021-07-05 20:50:41 +0200

    add cors configuration

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -5,7 +5,14 @@ import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   controllers: [TodoController],
-  imports: [GraphQLModule.forRoot({ autoSchemaFile: 'schema.gql' })],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      cors: {
+        origin: 'http://127.0.0.1:3001',
+      },
+    }),
+  ],
   providers: [TodoResolver],
   exports: [GraphQLModule],
 })
